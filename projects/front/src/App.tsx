d6c132e5e8410b190cdcb10ccd6ed000c55f4d79
Warubert <william.rubert@gmail.com>
2024-12-11 06:42:55 -0300

    feat: iniciada home

--- projects/front/src/App.tsx
+++ projects/front/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from './assets/logo.png';
 import './App.css'
 
 function App() {
@@ -9,25 +8,19 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <img src={logo} className="logo" alt="Vite logo" />
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <h1>Olá cinéfilo,</h1>
+        <h2>que bom te ver por aqui!</h2>
+
+        <div>
+          <h3>E-mail</h3>
+          <input type="text" placeholder="Insira seu e-mail"/>
+          <h3>Senha</h3>
+          <input type="text" placeholder="Insira sua senha"/>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
