c5cefcb17761351bf3a662123143c88c19b648a2
Warubert <william.rubert@gmail.com>
2024-12-11 06:44:07 -0300

    feat: iniciada home

--- projects/front/src/App.tsx
+++ projects/front/src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import logo from './assets/logo.png';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
