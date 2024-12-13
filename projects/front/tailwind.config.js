8a28f2581d3861eb56ef5f9fdad2b005859e113a
Warubert <william.rubert@gmail.com>
2024-12-12 23:46:16 -0300

    feat: finalizada página de login

---
+++ projects/front/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
